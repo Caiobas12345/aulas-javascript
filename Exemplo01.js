df28bebb065c7c804a3fa9c37948ce57ae16750e
CAIO DE OLIVEIRA <caio_oliveira151@estudante.sesisenai.org.br>
2026-09-08 22:15:09 -0300

    adição dos exemplos

--- Exemplo01.js
+++ Exemplo01.js
@@ -1,11 +1,8 @@
-if (true) {
-let local = 'visível apenas para você'
-var global = 'Vaza para fora'
+ if (true) {
+ let local = 'visível apenas para você'
+ var global = 'Vaza para fora'
+ }
 
-console.log(local); //Erro
-}
-
-//console.log(global); //Funciona
-
-
-
+    //console.log(local); //Erro
+ console.log(global); //Funciona
+ 
--- exemplo02.js
+++ exemplo02.js
@@ -1,4 +1,4 @@
-let dado = '42'; // tipo String
+let dado = 'Olá'; // tipo String
 console.log(dado);
 console.log (typeof dado);
 dado = 42;      //agora é number
