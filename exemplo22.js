62981b5b4e4f76cfc6a080b322ab7cbd09c6f5ce
CAIO DE OLIVEIRA <caio_oliveira151@estudante.sesisenai.org.br>
2026-09-18 20:17:44 -0300

    Atividade01.js

--- exemplo22.js
+++ exemplo22.js
@@ -2,6 +2,6 @@ function saudacao(nome = 'Visitante') {
     console.log(`Olá, ${nome}`);
 }
 
-// ⚠️ Você precisa incluir as linhas abaixo para a função rodar:
+
 saudacao('Clebsvaldo'); // Mostra no console: Olá, Carlos
 saudacao();         // Mostra no console: Olá, Visitante (usa o padrão)
